9ba0540181d9939e8d8d1e24bc2c93407c434ef4
Mathias <mathias@decodified.com>
2011-03-18 11:09:59 +0100

    Split out Examples, turn Spray module into root project

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,49 +1,31 @@
 import sbt._
 import Process._
 
-class Project(info: ProjectInfo) extends ParentProject(info) {
+class Project(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
   
+  // -------------------------------------------------------------------------------------------------------------------
+  // All repositories *must* go here! See ModuleConigurations below.
+  // -------------------------------------------------------------------------------------------------------------------
+  object Repositories {
+  }
+
   // -------------------------------------------------------------------------------------------------------------------
   // ModuleConfigurations
   // Every dependency that cannot be resolved from the built-in repositories (Maven Central and Scala Tools Releases)
   // must be resolved from a ModuleConfiguration. This will result in a significant acceleration of the update action.
   // Therefore, if repositories are defined, this must happen as def, not as val.
   // -------------------------------------------------------------------------------------------------------------------
-  lazy val parboiledModuleConfig   = ModuleConfiguration("org.parboiled", ScalaToolsSnapshots)
+  import Repositories._  
+  val parboiledModuleConfig = ModuleConfiguration("org.parboiled", ScalaToolsSnapshots)
   
   // -------------------------------------------------------------------------------------------------------------------
   // Dependencies
   // -------------------------------------------------------------------------------------------------------------------
-  object Deps {
-    lazy val parboiledC = "org.parboiled" % "parboiled-core" % "0.11.0-SNAPSHOT" % "compile" withSources()
-    lazy val parboiledS = "org.parboiled" % "parboiled-scala" % "0.11.0-SNAPSHOT" % "compile" withSources()
-    
-    lazy val specs   = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test" withSources()
-    lazy val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test" withSources()
-  }
+  override val akkaActor  = akkaModule("actor") withSources()
+  val akkaHttp            = akkaModule("http") withSources()
+  val parboiledC          = "org.parboiled" % "parboiled-core" % "0.11.0-SNAPSHOT" % "compile" withSources()
+  val parboiledS          = "org.parboiled" % "parboiled-scala" % "0.11.0-SNAPSHOT" % "compile" withSources()
   
-  // -------------------------------------------------------------------------------------------------------------------
-  // Subprojects
-  // -------------------------------------------------------------------------------------------------------------------
-  lazy val sprayProject     = project("Spray", "Spray", new SprayProject(_))
-  lazy val examplesProject = project("Examples", "Examples", new ExamplesProject(_))
-  
-  class SprayProject(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
-    override val akkaActor = akkaModule("actor") withSources()
-    val akkaHttp = akkaModule("http") withSources()
-    val parboiledC = Deps.parboiledC
-    val parboiledS = Deps.parboiledS
-    
-    val specs = Deps.specs
-    val mockito = Deps.mockito
-  }
-  
-  class ExamplesProject(info: ProjectInfo) extends DefaultWebProject(info) with AkkaProject {
-    override val akkaActor = akkaModule("actor") withSources()
-    val akkaHttp = akkaModule("http") withSources()
-    val spray = sprayProject
-    
-    val jetty7Server = "org.eclipse.jetty" % "jetty-server" % "8.0.0.M2" % "test"
-    val jetty7WebApp = "org.eclipse.jetty" % "jetty-webapp" % "8.0.0.M2" % "test"
-  }
+  val specs   = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test" withSources()
+  val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test" withSources()
 }
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -6,7 +6,7 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   // All repositories *must* go here! See ModuleConigurations below.
   // -------------------------------------------------------------------------------------------------------------------
   object Repositories {
-    lazy val akkaRepo    = MavenRepository("Akka Repository", "http://akka.io/repository")
+    val akkaRepo = MavenRepository("Akka Repository", "http://akka.io/repository")
   }
   
   // -------------------------------------------------------------------------------------------------------------------
@@ -16,10 +16,11 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   // Therefore, if repositories are defined, this must happen as def, not as val.
   // -------------------------------------------------------------------------------------------------------------------
   import Repositories._
-  lazy val akkaModuleConfig           = ModuleConfiguration("se.scalablesolutions.akka", akkaRepo)
+  lazy val akkaModuleConfig = ModuleConfiguration("se.scalablesolutions.akka", akkaRepo)
 
   // -------------------------------------------------------------------------------------------------------------------
   // Plugins
   // -------------------------------------------------------------------------------------------------------------------
   val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.0"
+  
 }
