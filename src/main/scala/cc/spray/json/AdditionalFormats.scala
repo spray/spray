f11476c6582e60aaa5b24174319b5a62de23c96b
Mathias <mathias@spray.cc>
2011-05-25 15:56:54 +0200

    Add DelegatingFormat

--- src/main/scala/cc/spray/json/AdditionalFormats.scala
+++ src/main/scala/cc/spray/json/AdditionalFormats.scala
@@ -27,6 +27,11 @@ trait AdditionalFormats {
     def read(value: JsValue) = value
   }
 
+  class DelegatingFormat[T](delegate: JsonFormat[T]) extends JsonFormat[T] {
+    def write(obj: T) = delegate.write(obj)
+    def read(json: JsValue) = delegate.read(json)
+  }
+
   def formatFromWriter[T :JsonWriter] = new JsonFormat[T] {
     def write(obj: T) = obj.toJson
     def read(value: JsValue) = throw new RuntimeException("JsonFormat constructed from JsonWriter can't read from JSON")
