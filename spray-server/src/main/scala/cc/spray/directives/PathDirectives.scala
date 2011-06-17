3099b903751bfaec19881381d2702613b014dc42
Mathias <mathias@spray.cc>
2011-06-17 12:41:07 +0200

    server: move number PathMatchers into PathDirectives.scala file

--- spray-server/src/main/scala/cc/spray/directives/PathDirectives.scala
+++ spray-server/src/main/scala/cc/spray/directives/PathDirectives.scala
@@ -125,7 +125,6 @@ private[spray] trait PathDirectives {
               "' must not contain more than one capturing group")
     }
   }
-  
 }
 
 /**
@@ -277,7 +276,7 @@ trait PathMatcher7[A, B, C, D, E, F, G] extends PathMatcher[(A, B, C, D, E, F, G
   def ~ (sub: PathMatcher0) = new Combi[(A, B, C, D, E, F, G)](this, sub) with PathMatcher7[A, B, C, D, E, F, G]
 }
 
-private[spray] class Combi[T <: Product](a: PathMatcher[_ <: Product], b: PathMatcher[_ <: Product]) extends PathMatcher[T]{
+private[directives] class Combi[T <: Product](a: PathMatcher[_ <: Product], b: PathMatcher[_ <: Product]) extends PathMatcher[T]{
   def apply(path: String) = a(path).flatMap {
     case (restA, capturesA) => b(restA).map {
       case (restB, capturesB) => (restB, (capturesA productJoin capturesB).asInstanceOf[T]) 
@@ -326,4 +325,95 @@ object PathEnd extends PathMatcher0 {
 
 object Remaining extends PathMatcher1[String] {
   def apply(path: String) = Some(("", Tuple1(path)))
+}
+
+/**
+ * A PathMatcher that efficiently matches a number of digits and extracts their (non-negative) Int value.
+ * The matcher will not match 0 digits or a sequence of digits that would represent an Int value larger
+ * than Int.MaxValue.
+ */
+object IntNumber extends NumberMatcher[Int](Int.MaxValue, 10) {
+  def fromChar(c: Char) = fromDecimalChar(c)
+}
+
+/**
+ * A PathMatcher that efficiently matches a number of digits and extracts their (non-negative) Long value.
+ * The matcher will not match 0 digits or a sequence of digits that would represent an Long value larger
+ * than Long.MaxValue.
+ */
+object LongNumber extends NumberMatcher[Long](Long.MaxValue, 10) {
+  def fromChar(c: Char) = fromDecimalChar(c)
+}
+
+/**
+ * A PathMatcher that efficiently matches a number of hex-digits and extracts their (non-negative) Int value.
+ * The matcher will not match 0 digits or a sequence of digits that would represent an Int value larger
+ * than Int.MaxValue.
+ */
+object HexIntNumber extends NumberMatcher[Int](Int.MaxValue, 16) {
+  def fromChar(c: Char) = fromHexChar(c)
+}
+
+/**
+ * A PathMatcher that efficiently matches a number of hex-digits and extracts their (non-negative) Long value.
+ * The matcher will not match 0 digits or a sequence of digits that would represent an Long value larger
+ * than Long.MaxValue.
+ */
+object HexLongNumber extends NumberMatcher[Long](Long.MaxValue, 16) {
+  def fromChar(c: Char) = fromHexChar(c)
+}
+
+// common implementation of Number matchers
+private[directives] abstract class NumberMatcher[@specialized(Int, Long) A](max: A, base: A)(implicit x: Integral[A])
+        extends PathMatcher1[A] {
+  import x._ // import implicit conversions for numeric operators
+  val minusOne = x.zero - x.one
+  val maxDivBase = max / base
+
+  def apply(path: String) = {
+    @tailrec
+    def digits(remainingPath: String, value: A): Option[(String, Tuple1[A])] = {
+      val a = if (remainingPath.isEmpty) minusOne else fromChar(remainingPath.charAt(0))
+      if (a == minusOne) {
+        if (value == minusOne) None else Some(remainingPath, Tuple1(value))
+      } else {
+        if (value == minusOne) {
+          digits(remainingPath.substring(1), a)
+        } else if (value <= maxDivBase && value * base <= max - a) { // protect from overflow
+          digits(remainingPath.substring(1), value * base + a)
+        } else None
+      }
+    }
+    digits(path, minusOne)
+  }
+
+  def fromChar(c: Char): A
+
+  def fromDecimalChar(c: Char): A = if ('0' <= c && c <= '9') (c - '0').asInstanceOf[A] else minusOne
+
+  def fromHexChar(c: Char): A = {
+    if ('0' <= c && c <= '9') (c - '0').asInstanceOf[A] else
+    if ('A' <= c && c <= 'F') (c - 'A' + 10).asInstanceOf[A] else
+    if ('a' <= c && c <= 'f') (c - 'a' + 10).asInstanceOf[A] else
+    minusOne
+  }
+}
+
+/**
+ * A PathMatcher that matches and extracts a Double value. The matched string representation is the pure decimal,
+ * optionally signed form of a double value, i.e. without exponent.
+ */
+object DoubleNumber extends PathMatcher1[Double] {
+  private val regexMatcher = new SimpleRegexMatcher("""[+-]?\d*\.?\d*""".r)
+
+  def apply(path: String) = {
+    // use 'flatMapValue' on the result of any PathMatcher1 to convert the extracted value to another type
+    regexMatcher(path).flatMapValue { string =>
+      try {
+        Some(java.lang.Double.parseDouble(string))
+      } catch {
+        case _: NumberFormatException => None
+      }
+    }
+  }
 }
