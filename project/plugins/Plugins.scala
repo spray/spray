06742a78ee3bf0e34ec893754f19f5e2b56b82d3
Mathias <mathias@spray.cc>
2011-07-11 14:46:22 +0200

    Add implicit.ly integration stubs

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -16,11 +16,12 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   // Therefore, if repositories are defined, this must happen as def, not as val.
   // -------------------------------------------------------------------------------------------------------------------
   import Repositories._
-  lazy val akkaModuleConfig = ModuleConfiguration("se.scalablesolutions.akka", AkkaRepo)
+  val akkaModuleConfig = ModuleConfiguration("se.scalablesolutions.akka", AkkaRepo)
+  val posterousSbtModuleConfig = ModuleConfiguration("net.databinder", ScalaToolsReleases)
 
   // -------------------------------------------------------------------------------------------------------------------
   // Plugins
   // -------------------------------------------------------------------------------------------------------------------
   val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1.3"
-  
+  val posterousSbt = "net.databinder" % "posterous-sbt" % "0.1.7"
 }
