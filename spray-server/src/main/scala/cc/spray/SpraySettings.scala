6d2a86aba984c4bcd50198900d7cbfc3016f1b15
Mathias <mathias@spray.cc>
2011-07-26 15:45:11 +0200

    server: rename the 'CloseConnection' config section key

--- spray-server/src/main/scala/cc/spray/SpraySettings.scala
+++ spray-server/src/main/scala/cc/spray/SpraySettings.scala
@@ -21,7 +21,7 @@ import akka.config.Config._
 object SpraySettings {
   lazy val RootActorId         = config.getString("spray.root-actor-id", "spray-root-service")
   lazy val AsyncTimeout        = config.getInt("spray.timeout", 1000)
-  lazy val CloseConnection     = config.getBool("spray.close-connection", true)
+  lazy val CloseConnection     = config.getBool("spray.set-connection-close-header", true)
   lazy val SetDateHeader       = config.getBool("spray.set-date-header", false)
   lazy val RootPath            = config.getString("spray.root-path")
   lazy val CompactJsonPrinting = config.getBool("spray.compact-json-printing", false)
