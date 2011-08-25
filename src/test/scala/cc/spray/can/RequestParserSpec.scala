537c300f5515062f78f897302979f27514e68098
Mathias <mathias@spray.cc>
2011-08-25 10:47:49 +0200

    Complete request parsing test cases

--- src/test/scala/cc/spray/can/RequestParserSpec.scala
+++ src/test/scala/cc/spray/can/RequestParserSpec.scala
@@ -26,47 +26,121 @@ class RequestParserSpec extends Specification {
   val HH = HttpHeader
 
   "The request parsing logic" should {
+    "properly parse a request" in {
+      "without headers and body" in {
+        test {
+          """|GET / HTTP/1.1
+             |
+             |"""
+        } mustEqual ("GET", "/", Nil, "")
+      }
 
-    "properly parse request example 1" in {
-      test {
-        """|GET / HTTP/1.1
-           |Host: api.example.com
-           |
-           |"""
-      } mustEqual ("GET", "/", List(HH("Host", "api.example.com")), "")
+      "with one header" in {
+        test {
+          """|GET / HTTP/1.1
+             |Host: api.example.com
+             |
+             |"""
+        } mustEqual ("GET", "/", List(HH("Host", "api.example.com")), "")
+      }
+
+      "with 3 headers and a body" in {
+        test {
+          """|POST /resource/yes HTTP/1.1
+             |User-Agent: curl/7.19.7 xyz
+             |Transfer-Encoding:identity
+             |Content-Length    : 17
+             |
+             |Shake your BOODY!"""
+        } mustEqual ("POST", "/resource/yes", List(
+          HH("Content-Length", "17"),
+          HH("Transfer-Encoding", "identity"),
+          HH("User-Agent", "curl/7.19.7 xyz")
+        ), "Shake your BOODY!")
+      }
+
+      "with multi-line headers" in {
+        test {
+          """|DELETE /abc HTTP/1.1
+             |User-Agent: curl/7.19.7
+             | abc
+             |    xyz
+             |Accept
+             | : */*  """ + """
+             |
+             |"""
+        } mustEqual ("DELETE", "/abc", List(
+          HH("Accept", "*/*"),
+          HH("User-Agent", "curl/7.19.7 abc xyz")
+        ), "")
+      }
     }
 
-    "properly parse request example 2" in {
-      test {
-        """|POST /resource/yes HTTP/1.1
-           |User-Agent: curl/7.19.7 xyz
-           |Accept:*/*
-           |Content-Length    : 17
-           |
-           |Shake your BOODY!"""
-      } mustEqual ("POST", "/resource/yes", List(
-        HH("Content-Length", "17"),
-        HH("Accept", "*/*"),
-        HH("User-Agent", "curl/7.19.7 xyz")
-      ), "Shake your BOODY!")
-    }
+    "reject a request with" in {
+      "an illegal HTTP method" in {
+        test("get") mustEqual ErrorRequestParser(501, "Unsupported HTTP method")
+        test("GETX") mustEqual ErrorRequestParser(501, "Unsupported HTTP method")
+      }
 
-    "properly parse request example 3" in {
-      test {
-        """|DELETE /abc HTTP/1.1
-           |User-Agent: curl/7.19.7
-           | abc
-           |    xyz
-           |Accept
-           | : */*  """ + """
-           |
-           |"""
-      } mustEqual ("DELETE", "/abc", List(
-        HH("Accept", "*/*"),
-        HH("User-Agent", "curl/7.19.7 abc xyz")
-      ), "")
-    }
+      "an URI longer than 2048 chars" in {
+        test("GET x" + "xxxx" * 512) mustEqual ErrorRequestParser(414, "URIs with more than 2048 characters are not supported by this server")
+      }
 
+      "HTTP version 1.0" in {
+        test("GET / HTTP/1.0\r\n") mustEqual ErrorRequestParser(400, "Http version 'HTTP/1.0' not supported")
+      }
+
+      "with an illegal char in a header name" in {
+        test {
+          """|GET / HTTP/1.1
+             |User@Agent: curl/7.19.7"""
+        } mustEqual ErrorRequestParser(400, "Invalid character '@', expected TOKEN CHAR, LWS or COLON")
+      }
+
+      "with a header name longer than 64 chars" in {
+        test {
+          """|GET / HTTP/1.1
+             |UserxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxAgent: curl/7.19.7"""
+        } mustEqual ErrorRequestParser(400, "HTTP headers with names longer than 64 characters are not supported by this server")
+      }
+
+      "with a non-identity transfer encoding" in {
+        test {
+          """|GET / HTTP/1.1
+             |Transfer-Encoding: chunked
+             |
+             |abc"""
+        } mustEqual ErrorRequestParser(501, "Non-identity transfer encodings are not currently supported by this server")
+      }
+
+      "with a header-value longer than 8192 chars" in {
+        test {
+          """|GET / HTTP/1.1
+             |Fancy: 0""" + ("12345678" * 1024)
+        } mustEqual ErrorRequestParser(400, "HTTP header values longer than 8192 characters are not supported by this server (header 'Fancy')")
+      }
+
+      "with an invalid Content-Length header value" in {
+        test {
+          """|GET / HTTP/1.1
+             |Content-Length: 1.5
+             |
+             |abc"""
+        } mustEqual ErrorRequestParser(400, "Invalid Content-Length header value")
+        test {
+          """|GET / HTTP/1.1
+             |Content-Length: -3
+             |
+             |abc"""
+        } mustEqual ErrorRequestParser(400, "Invalid Content-Length header value")
+        test {
+          """|GET / HTTP/1.1
+             |Content-Length: 3
+             |
+             |abcde"""
+        } mustEqual ErrorRequestParser(400, "Illegal Content-Length: request entity is longer than Content-Length header value")
+      }
+    }
   }
 
   def test(request: String) = {
