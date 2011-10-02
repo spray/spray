674e11694e63cadae5ea5d114ff33068c6a13613
Mathias <mathias@spray.cc>
2011-10-02 22:32:08 +0200

    Fix incorrect timeout-actor-id default setting

--- spray-server/src/main/scala/cc/spray/SpraySettings.scala
+++ spray-server/src/main/scala/cc/spray/SpraySettings.scala
@@ -20,7 +20,7 @@ import akka.config.Config._
 
 object SpraySettings {
   lazy val RootActorId         = config.getString("spray.root-actor-id", "spray-root-service")
-  lazy val TimeoutActorId      = config.getString("spray.timeout-actor-id", "spray-timeout-actor")
+  lazy val TimeoutActorId      = config.getString("spray.timeout-actor-id", "spray-root-service")
   lazy val AsyncTimeout        = config.getInt("spray.request-timeout", 1000)
   lazy val RootPath            = config.getString("spray.root-path")
   lazy val CompactJsonPrinting = config.getBool("spray.compact-json-printing", false)
