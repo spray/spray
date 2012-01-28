6a4ff67713b5735dcd4690e079d3bdfe930e228c
Mathias <mathias@spray.cc>
2012-01-28 22:07:29 +0100

    ReadmeSpec: streamline and add missing test

--- src/test/scala/cc/spray/json/ReadmeSpec.scala
+++ src/test/scala/cc/spray/json/ReadmeSpec.scala
@@ -40,22 +40,19 @@ class ReadmeSpec extends Specification {
   }
   
   case class Color(name: String, red: Int, green: Int, blue: Int)
+  val color = Color("CadetBlue", 95, 158, 160)
   
   "The case class example" should {
     "behave as expected" in {
       object MyJsonProtocol extends DefaultJsonProtocol {
-        implicit val colorFormat = jsonFormat(Color, "name", "red", "green", "blue")
+        implicit val colorFormat = jsonFormat4(Color)
       }      
       import MyJsonProtocol._
-      
-      val json = Color("CadetBlue", 95, 158, 160).toJson
-      val color = json.as[Color]
-      
-      color mustEqual Color("CadetBlue", 95, 158, 160)
+      color.toJson.as[Color] mustEqual color
     }
   }
   
-  "The non case class example" should {
+  "The non case class (array) example" should {
     "behave as expected" in {
       object MyJsonProtocol extends DefaultJsonProtocol {
         implicit object ColorJsonFormat extends JsonFormat[Color] {
@@ -70,12 +67,33 @@ class ReadmeSpec extends Specification {
         }
       }      
       import MyJsonProtocol._
-      
-      val json = Color("CadetBlue", 95, 158, 160).toJson
-      val color = json.as[Color]
-      
-      color mustEqual Color("CadetBlue", 95, 158, 160)
+      color.toJson.as[Color] mustEqual color
     }
   }
+
+  "The non case class (object) example" should {
+      "behave as expected" in {
+        object MyJsonProtocol extends DefaultJsonProtocol {
+          implicit object ColorJsonFormat extends JsonFormat[Color] {
+            def write(c: Color) = JsObject(
+              "name" -> JsString(c.name),
+              "red" -> JsNumber(c.red),
+              "green" -> JsNumber(c.green),
+              "blue" -> JsNumber(c.blue)
+            )
+            def read(json: JsValue) = {
+              for {
+                name: String <- json("name")
+                red: Int <- json("red")
+                green: Int <- json("green")
+                blue: Int <- json("blue")
+              } yield Color(name, red, green, blue)
+            }
+          }
+        }
+        import MyJsonProtocol._
+        color.toJson.as[Color] mustEqual color
+      }
+    }
   
 }
