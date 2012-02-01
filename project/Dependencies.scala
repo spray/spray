1692ff49a2321ace99eb03fb3cfc853cd81c5157
Mathias <mathias@spray.cc>
2012-02-01 14:53:19 +0100

    Upgrade to akka 1.3, slf4j 1.6.4 and specs2 1.7.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,12 +8,12 @@ object Dependencies {
   )
 
   object V {
-    val akka      = "1.3-RC4"
-    val logback   = "0.9.29"
+    val akka      = "1.3"
+    val logback   = "1.0.0"
   }
 
   object Compile {
-    val slf4j         = "org.slf4j"                 %   "slf4j-api"                 % "1.6.1"     % "compile"
+    val slf4j         = "org.slf4j"                 %   "slf4j-api"                 % "1.6.4"     % "compile"
     val akkaActor     = "se.scalablesolutions.akka" %   "akka-actor"                % V.akka      % "compile"
   }
 
@@ -22,7 +22,7 @@ object Dependencies {
   }
 
   object Test {
-    val specs2        = "org.specs2"                %%  "specs2"                    % "1.6.1"     % "test"
+    val specs2        = "org.specs2"                %%  "specs2"                    % "1.7.1"     % "test"
     val akkaSlf4j     = "se.scalablesolutions.akka" %   "akka-slf4j"                % V.akka      % "test"
     val logback       = "ch.qos.logback"            %   "logback-classic"           % V.logback   % "test"
   }
