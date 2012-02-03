a3d22aad94882c454b009ef8583b3be92917f982
Mathias <mathias@spray.cc>
2012-02-03 16:57:15 +0100

    Bump version to 0.9.0-RC1, clean up buildfile

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,9 +6,13 @@ import xml.transform.{RewriteRule, RuleTransformer}
 object BuildSettings {
 
   lazy val basicSettings = Seq[Setting[_]](
+    version       := "0.9.0-RC1",
+    homepage      := Some(new URL("http://spray.cc")),
     organization  := "cc.spray",
-    version       := "0.9.0-SNAPSHOT",
-    description   := "a suite of lightweight Scala libraries for building and consuming RESTful web services on top of Akka",
+    organizationHomepage := Some(new URL("http://spray.cc")),
+    description   := "A suite of lightweight Scala libraries for building and consuming RESTful web services on top of Akka",
+    startYear     := Some(2011),
+    licenses      := Seq("Apache 2" -> new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")),
     scalaVersion  := "2.9.1",
     resolvers     ++= Dependencies.resolutionRepos,
     scalacOptions := Seq("-deprecation", "-encoding", "utf8")
@@ -24,35 +28,14 @@ object BuildSettings {
     publishMavenStyle := true,
     publishTo <<= version { version =>
       Some {
-        "snapshots" at {
-          "http://nexus.scala-tools.org/content/repositories/" + {
+        "spray repo" at {
+          // public uri is repo.spray.cc, we use an SSH tunnel to the nexus here
+          "http://localhost:42424/content/repositories/" + {
             if (version.trim.endsWith("SNAPSHOT")) "snapshots/" else"releases/"
           }
         }
       }
-    },
-    pomExtra := (
-      <url>http://spray.cc/</url>
-      <inceptionYear>2011</inceptionYear>
-      <licenses>
-        <license>
-          <name>Apache 2</name>
-          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-          <distribution>repo</distribution>
-        </license>
-      </licenses>
-      <developers>
-        <developer>
-          <id>sirthias</id>
-          <name>Mathias Doenitz</name>
-          <timezone>+1</timezone>
-          <email>mathias [at] spray.cc</email>
-        </developer>
-      </developers>
-      <scm>
-        <url>http://github.com/spray/</url>
-      </scm>
-    )
+    }
   )
 
   lazy val noPublishing = Seq(
