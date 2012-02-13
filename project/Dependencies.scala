e7e9ac7be69e97d554a8fa1cce819ae1c59ec1d4
Mathias <mathias@spray.cc>
2012-02-13 11:14:14 +0100

    Upgrade to Akka 2.0-M4, specs2 1.7.1 and SLF4J 1.6.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,33 +3,32 @@ import sbt._
 object Dependencies {
 
   val resolutionRepos = Seq(
-    "Akka Repository" at "http://akka.io/repository/",
-    ScalaToolsSnapshots
+    "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   )
 
   object V {
-    val akka      = "1.3-RC4"
-    val logback   = "0.9.29"
+    val akka      = "2.0-M4"
+    val logback   = "1.0.0"
   }
 
   object Compile {
-    val slf4j         = "org.slf4j"                 %   "slf4j-api"                 % "1.6.1"     % "compile"
-    val akkaActor     = "se.scalablesolutions.akka" %   "akka-actor"                % V.akka      % "compile"
+    val slf4j       = "org.slf4j"         %   "slf4j-api"       % "1.6.4"     % "compile"
+    val akkaActor   = "com.typesafe.akka" %   "akka-actor"      % V.akka      % "compile"
   }
 
   object Provided {
-    val akkaActor     = "se.scalablesolutions.akka" %   "akka-actor"                % V.akka      % "provided"
+    val akkaActor   = "com.typesafe.akka" %   "akka-actor"      % V.akka      % "provided"
   }
 
   object Test {
-    val specs2        = "org.specs2"                %%  "specs2"                    % "1.6.1"     % "test"
-    val akkaSlf4j     = "se.scalablesolutions.akka" %   "akka-slf4j"                % V.akka      % "test"
-    val logback       = "ch.qos.logback"            %   "logback-classic"           % V.logback   % "test"
+    val specs2      = "org.specs2"        %%  "specs2"          % "1.7.1"     % "test"
+    val akkaSlf4j   = "com.typesafe.akka" %   "akka-slf4j"      % V.akka      % "test"
+    val logback     = "ch.qos.logback"    %   "logback-classic" % V.logback   % "test"
   }
 
   object Runtime {
-    val akkaSlf4j     = "se.scalablesolutions.akka" %   "akka-slf4j"                % V.akka      % "runtime"
-    val logback       = "ch.qos.logback"            %   "logback-classic"           % V.logback   % "runtime"
+    val akkaSlf4j   = "com.typesafe.akka" %   "akka-slf4j"      % V.akka      % "runtime"
+    val logback     = "ch.qos.logback"    %   "logback-classic" % V.logback   % "runtime"
   }
 
 }
