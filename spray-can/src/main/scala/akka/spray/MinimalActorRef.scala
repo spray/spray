e7ecc23f0bbd1d4d3d9c2be70f57a28f068c48fb
Mathias <mathias@spray.cc>
2012-03-20 16:45:50 +0100

    can: cleanup

---
+++ spray-can/src/main/scala/akka/spray/MinimalActorRef.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2011, 2012 Mathias Doenitz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package akka.spray
+
+import akka.actor.{InternalActorRef, ActorRef}
+
+/**
+ * We need access to a minimal wrapper ActorRef in order to associate messages with
+ * their replies transparently to the API user. The akka.actor.MinimalActorRef is
+ * perfect for this but, unfortunately, is marked `private[akka]`, which is why
+ * we need to "expose" it here.
+ */
+abstract class MinimalActorRef(related: ActorRef) extends akka.actor.MinimalActorRef {
+  lazy val path = provider.tempPath()
+  def provider = related.asInstanceOf[InternalActorRef].provider
+}
