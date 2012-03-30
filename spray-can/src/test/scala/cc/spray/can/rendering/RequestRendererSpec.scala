6c48b246e451fb869bf40fa57177478026b6e459
Mathias <mathias@spray.cc>
2012-03-30 16:14:10 +0200

    test: simplify tests

--- spray-can/src/test/scala/cc/spray/can/rendering/RequestRendererSpec.scala
+++ spray-can/src/test/scala/cc/spray/can/rendering/RequestRendererSpec.scala
@@ -17,102 +17,102 @@
 package cc.spray.can
 package rendering
 
-import org.specs2._
 import model._
 import HttpMethods._
+import org.specs2.mutable.Specification
 
-class RequestRendererSpec extends Specification { def is =
+class RequestRendererSpec extends Specification {
 
-    "The request preparation logic should properly render a" ^
-      "GET request without headers and without body" ! e1 ^
-      "POST request, a few headers and no body" ! e2 ^
-      "PUT request, a few headers and a body" ! e3 ^
-      "PUT request start (chunked) without body" ! e4 ^
-      "POST request start (chunked) with body" ! e5
+  "The request preparation logic" should {
+    "properly render a" in {
 
+      "GET request without headers and without body" in {
+        HttpRequest(method = GET, uri = "/abc") must beRenderedTo {
+          """|GET /abc HTTP/1.1
+             |Host: test.com:8080
+             |User-Agent: spray-can/1.0.0
+             |
+             |"""
+        }
+      }
 
-  def e1 = prep(HttpRequest(method = GET, uri = "/abc")) mustEqual prep {
-    """|GET /abc HTTP/1.1
-       |Host: test.com:8080
-       |User-Agent: spray-can/1.0.0
-       |
-       |"""
-  }
+      "POST request, a few headers and no body" in {
+        HttpRequest(
+          method = POST,
+          uri = "/abc/xyz",
+          headers = List(
+            HttpHeader("X-Fancy", "naa"),
+            HttpHeader("Age", "0")
+          )
+        ) must beRenderedTo {
+          """|POST /abc/xyz HTTP/1.1
+             |X-Fancy: naa
+             |Age: 0
+             |Host: test.com:8080
+             |User-Agent: spray-can/1.0.0
+             |
+             |"""
+        }
+      }
 
-  def e2 = prep {
-    HttpRequest(
-      method = POST,
-      uri = "/abc/xyz",
-      headers = List(
-        HttpHeader("X-Fancy", "naa"),
-        HttpHeader("Age", "0")
-      )
-    )
-  } mustEqual prep {
-    """|POST /abc/xyz HTTP/1.1
-       |X-Fancy: naa
-       |Age: 0
-       |Host: test.com:8080
-       |User-Agent: spray-can/1.0.0
-       |
-       |"""
-  }
+      "PUT request, a few headers and a body" in {
+        HttpRequest(
+          method = PUT,
+          uri = "/abc/xyz",
+          headers = List(
+            HttpHeader("X-Fancy", "naa"),
+            HttpHeader("Cache-Control", "public")
+          ),
+          body = "The content please!".getBytes("ISO-8859-1")
+        ) must beRenderedTo {
+          """|PUT /abc/xyz HTTP/1.1
+             |X-Fancy: naa
+             |Cache-Control: public
+             |Host: test.com:8080
+             |User-Agent: spray-can/1.0.0
+             |Content-Length: 19
+             |
+             |The content please!"""
+        }
+      }
 
-  def e3 = prep {
-    HttpRequest(
-      method = PUT,
-      uri = "/abc/xyz",
-      headers = List(
-        HttpHeader("X-Fancy", "naa"),
-        HttpHeader("Cache-Control", "public")
-      ),
-      body = "The content please!".getBytes("ISO-8859-1")
-    )
-  } mustEqual prep {
-    """|PUT /abc/xyz HTTP/1.1
-       |X-Fancy: naa
-       |Cache-Control: public
-       |Host: test.com:8080
-       |User-Agent: spray-can/1.0.0
-       |Content-Length: 19
-       |
-       |The content please!"""
-  }
+      "PUT request start (chunked) without body" in {
+        ChunkedRequestStart(HttpRequest(PUT, "/abc/xyz")) must beRenderedTo {
+          """|PUT /abc/xyz HTTP/1.1
+             |Host: test.com:8080
+             |User-Agent: spray-can/1.0.0
+             |Transfer-Encoding: chunked
+             |
+             |"""
+        }
+      }
 
-  def e4 = prep {
-    ChunkedRequestStart(HttpRequest(PUT, "/abc/xyz"))
-  } mustEqual prep {
-    """|PUT /abc/xyz HTTP/1.1
-       |Host: test.com:8080
-       |User-Agent: spray-can/1.0.0
-       |Transfer-Encoding: chunked
-       |
-       |"""
-  }
-
-  def e5 = prep {
-    ChunkedRequestStart(HttpRequest(POST, "/abc/xyz").withBody("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-  } mustEqual prep {
-    """|POST /abc/xyz HTTP/1.1
-       |Host: test.com:8080
-       |User-Agent: spray-can/1.0.0
-       |Transfer-Encoding: chunked
-       |
-       |1a
-       |ABCDEFGHIJKLMNOPQRSTUVWXYZ
-       |"""
+      "POST request start (chunked) with body" in {
+        ChunkedRequestStart(HttpRequest(POST, "/abc/xyz")
+          .withBody("ABCDEFGHIJKLMNOPQRSTUVWXYZ")) must beRenderedTo {
+          """|POST /abc/xyz HTTP/1.1
+             |Host: test.com:8080
+             |User-Agent: spray-can/1.0.0
+             |Transfer-Encoding: chunked
+             |
+             |1a
+             |ABCDEFGHIJKLMNOPQRSTUVWXYZ
+             |"""
+        }
+      }
+    }
   }
 
   val renderer = new RequestRenderer("spray-can/1.0.0", 256)
 
-  def prep(part: HttpRequestPart) = {
-    val RenderedMessagePart(buffers, false) = renderer.render {
-      HttpRequestPartRenderingContext(part, "test.com", 8080)
+  def beRenderedTo(content: String) = {
+    beEqualTo(content.stripMargin.replace("\n", "\r\n")) ^^ { part: HttpRequestPart =>
+      val RenderedMessagePart(buffers, false) = renderer.render {
+        HttpRequestPartRenderingContext(part, "test.com", 8080)
+      }
+      val sb = new java.lang.StringBuilder()
+      buffers.foreach { buf => while (buf.remaining > 0) sb.append(buf.get.toChar) }
+      sb.toString
     }
-    val sb = new java.lang.StringBuilder()
-    buffers.foreach { buf => while (buf.remaining > 0) sb.append(buf.get.toChar) }
-    sb.toString
   }
-
-  def prep(request: String) = request.stripMargin.replace("\n", "\r\n")
 }
