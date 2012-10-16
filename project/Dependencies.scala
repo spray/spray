c9887f12eed9e7b059a718436efd3389a7ce18e9
Mathias <mathias@decodified.com>
2012-10-16 14:36:26 +0200

    sbt: upgrade to parboiled 1.1.3 and spray-json 1.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,13 +22,13 @@ object Dependencies {
   val liftJson      = "net.liftweb"                             %   "lift-json_2.9.1"             % "2.4"
   val logback       = "ch.qos.logback"                          %   "logback-classic"             % "1.0.0"
   val mimepull      = "org.jvnet.mimepull"                      %   "mimepull"                    % "1.8"
-  val parboiled     = "org.parboiled"                           %%  "parboiled-scala"             % "1.1.2"
+  val parboiled     = "org.parboiled"                           %%  "parboiled-scala"             % "1.1.3"
   val pegdown       = "org.pegdown"                             %   "pegdown"                     % "1.1.0"
   val scalate       = "org.fusesource.scalate"                  %   "scalate-core"                % "1.5.3"
   val shapeless     = "com.chuusai"                             %%  "shapeless"                   % "1.2.2"
   val scalatest     = "org.scalatest"                           %%  "scalatest"                   % "1.8"
   val specs2        = "org.specs2"                              %%  "specs2"                      % "1.12.2"
-  val sprayJson     = "io.spray"                                %%  "spray-json"                  % "1.2.1"
+  val sprayJson     = "io.spray"                                %%  "spray-json"                  % "1.2.2"
   val twirlApi      = "io.spray"                                %%  "twirl-api"                   % "0.6.0"
 
 }
