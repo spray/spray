7bf5ab93dae6896a26bb891fcbd530eb9ed49a8a
Paul Snively <psnively@mac.com>
2012-11-01 15:03:07 -0700

    Made HttpService.routingSettings an implicit def to correctly support overriding.

--- spray-routing/src/main/scala/spray/routing/HttpService.scala
+++ spray-routing/src/main/scala/spray/routing/HttpService.scala
@@ -43,7 +43,7 @@ trait HttpService extends Directives {
    * Normally you configure via the application.conf on the classpath,
    * but you can also override this member.
    */
-  implicit val routingSettings = RoutingSettings.Default
+  implicit def routingSettings = RoutingSettings.Default
 
   val log = LoggingContext.fromActorRefFactory
 
