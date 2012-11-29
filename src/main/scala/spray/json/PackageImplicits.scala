2c14d4250fcd3888a21b1a09633a8eca343d696f
Johannes Rudolph <johannes_rudolph@gmx.de>
2012-11-29 17:07:45 +0100

    a simple builder API to create JsObject values from tuple2s

--- src/main/scala/spray/json/PackageImplicits.scala
+++ src/main/scala/spray/json/PackageImplicits.scala
@@ -3,5 +3,6 @@ package spray.json
 trait PackageImplicits
   extends JsValuePrintingImplicits
   with JsValueReadingImplicits
+  with JsObjectBuildingImplicits
   with lenses.ExtraImplicits
   with DynamicJsValueImplicits
