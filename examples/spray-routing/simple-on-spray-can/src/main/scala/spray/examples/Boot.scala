f395281b06a48d85b0b3ad0afe247322e0a5a6af
Mathias <mathias@spray.io>
2012-11-30 22:36:17 +0100

    can: fix broken overridability of SprayCanHttpServerApp.system member

--- examples/spray-routing/simple-on-spray-can/src/main/scala/spray/examples/Boot.scala
+++ examples/spray-routing/simple-on-spray-can/src/main/scala/spray/examples/Boot.scala
@@ -9,6 +9,6 @@ object Boot extends App with SprayCanHttpServerApp {
   // create and start our service actor
   val service = system.actorOf(Props[DemoServiceActor], "demo-service")
 
-  // create a new HttpServer using our handler tell it where to bind to
+  // create a new HttpServer using our handler and tell it where to bind to
   newHttpServer(service) ! Bind(interface = "localhost", port = 8080)
 }
