0022383a933e007123569da183374ec9043169ac
Mathias <mathias@spray.io>
2012-11-30 12:09:45 +0100

    httpx & routing: remove work-arounds for https://issues.scala-lang.org/browse/SI-5031 after its fix

--- spray-routing/src/main/scala/spray/routing/Route.scala
+++ spray-routing/src/main/scala/spray/routing/Route.scala
@@ -14,15 +14,19 @@
  * limitations under the License.
  */
 
-package spray
+package spray.routing
 
-import shapeless.HNil
+import shapeless.HList
 
 
-package object routing {
+object Route {
+  def apply(f: Route): Route = f
 
-  type Route = RequestContext => Unit
-  type RouteGenerator[T] = T => Route
-  type Directive0 = Directive[HNil]
-
+  /**
+   * Converts the route into a directive that never passes the request to its inner route
+   * (and always returns its underlying route).
+   */
+  def toDirective[L <: HList](route: Route): Directive[L] = new Directive[L] {
+    def happly(f: L => Route) = route
+  }
 }
