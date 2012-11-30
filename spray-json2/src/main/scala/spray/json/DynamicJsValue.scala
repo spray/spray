5b03cb9a34063097f226c5e4289e40c2d35d2a8b
Johannes Rudolph <johannes_rudolph@gmx.de>
2012-11-30 12:17:16 +0100

    spray-json: provide selectDynamic for scala 2.10.x

--- spray-json2/src/main/scala/spray/json/DynamicJsValue.scala
+++ spray-json2/src/main/scala/spray/json/DynamicJsValue.scala
@@ -3,6 +3,9 @@ package spray.json
 case class DynamicJsValue(_value: JsValue) extends Dynamic {
   def applyDynamic(key: String)(index: Int = -1): DynamicValidatedJsValue =
     (if (index == -1) _value(key) else _value(key).flatMap(_(index))).dyn
+
+  def selectDynamic(key: String): DynamicValidatedJsValue =
+    _value(key).dyn
 }
 
 object DynamicJsValue {
--- spray-json2/src/main/scala/spray/json/DynamicValidatedJsValue.scala
+++ spray-json2/src/main/scala/spray/json/DynamicValidatedJsValue.scala
@@ -3,6 +3,9 @@ package spray.json
 case class DynamicValidatedJsValue(value: Validated[JsValue]) extends Dynamic  {
   def applyDynamic(key: String)(index: Int = -1): DynamicValidatedJsValue =
     value.flatMap(_.dyn.applyDynamic(key)(index)).dyn
+
+  def selectDynamic(key: String): DynamicValidatedJsValue =
+    value.flatMap(_.dyn.selectDynamic(key)).dyn
 }
 
 object DynamicValidatedJsValue {
