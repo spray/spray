48db799f8a5436ace945eed916d75b9e6da27df1
Mathias <mathias@spray.io>
2012-12-04 11:39:28 +0100

    can: improve flexibility of HttpClient and HttpDialog to specify custom SSL-enabling-from-tag functions

--- spray-can/src/main/scala/spray/can/client/HttpDialog.scala
+++ spray-can/src/main/scala/spray/can/client/HttpDialog.scala
@@ -32,7 +32,7 @@ import spray.http._
  */
 object HttpDialog {
   private sealed abstract class Action
-  private case class ConnectAction(host: String, port: Int, ssl: Boolean) extends Action
+  private case class ConnectAction(host: String, port: Int, tag: Any) extends Action
   private case class SendAction(request: HttpRequest) extends Action
   private case class WaitIdleAction(duration: Duration) extends Action
   private case class ReplyAction(f: HttpResponse => HttpRequest) extends Action
@@ -50,9 +50,9 @@ object HttpDialog {
       context.stop(self)
     }
 
-    def receive = {
-      case ConnectAction(host, port, ssl) :: remainingActions =>
-        val command = HttpClient.Connect(host, port, if (ssl) HttpClient.SslEnabled else ())
+    def receive: Receive = {
+      case ConnectAction(host, port, tag) :: remainingActions =>
+        val command = HttpClient.Connect(host, port, tag)
         client.tell(command, Reply.withContext(remainingActions))
 
       case SendAction(request) :: remainingActions =>
@@ -226,9 +226,9 @@ object HttpDialog {
   /**
    * Constructs a new `HttpDialog` for a connection to the given host and port.
    */
-  def apply(httpClient: ActorRef, host: String, port: Int = 80, ssl: Boolean = false)
+  def apply(httpClient: ActorRef, host: String, port: Int = 80, tag: Any = ())
            (implicit refFactory: ActorRefFactory) =
-    new SendFirst(new Context(refFactory, httpClient, ConnectAction(host, port, ssl)))
+    new SendFirst(new Context(refFactory, httpClient, ConnectAction(host, port, tag)))
       with SendMany
       with WaitIdle
 
