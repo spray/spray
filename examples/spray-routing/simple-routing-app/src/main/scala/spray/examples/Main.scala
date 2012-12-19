de5ce8bd55812d5dee4cb19119c2177a5fba0a89
Mathias <mathias@spray.io>
2012-12-19 17:00:31 +0100

    examples: improve simple-routing-app example

--- examples/spray-routing/simple-routing-app/src/main/scala/spray/examples/Main.scala
+++ examples/spray-routing/simple-routing-app/src/main/scala/spray/examples/Main.scala
@@ -18,26 +18,27 @@ object Main extends App with SimpleRoutingApp {
 //      path("") {
 //        redirect("/hello")
 //      } ~
-//      path("hello") {
-//        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+//        path("hello") {
+//          respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+//            complete {
+//              <html>
+//                <h1>Say hello to <em>spray</em> on <em>spray-can</em>!</h1>
+//                <p>(<a href="/stop?method=post">stop server</a>)</p>
+//              </html>
+//            }
+//          }
+//        }
+//    } ~
+//      (post | parameter('method ! "post")) {
+//        path("stop") {
 //          complete {
-//            <html>
-//              <h1>Say hello to <em>spray</em> on <em>spray-can</em>!</h1>
-//              <p>(<a href="/stop?method=post">stop server</a>)</p>
-//            </html>
+//            system.scheduler.scheduleOnce(Duration(1, "sec")) {
+//              system.shutdown()
+//            }
+//            "Shutting down in 1 second..."
 //          }
 //        }
 //      }
-//    } ~
-//    (post | parameter('method ! "post")) {
-//      path("stop") {
-//        complete {
-//          val responseFuture = Promise.successful("Shutting down in 1 second...").future
-//          responseFuture.delay(Duration(1, "sec")).onComplete(_ => system.shutdown())
-//          responseFuture
-//        }
-//      }
-//    }
 //  }
 
 }
