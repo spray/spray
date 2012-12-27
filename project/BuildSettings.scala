7c8e0742dc5a8493a7a780b1fade198bee1d63ed
Mathias <mathias@spray.io>
2012-12-27 11:12:03 +0100

    sbt: upgrade to Scala 2.10.0 and latest versions of dependencies

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -15,7 +15,7 @@ object BuildSettings {
                              "web services on top of Akka",
     startYear             := Some(2011),
     licenses              := Seq("Apache 2" -> new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")),
-    scalaVersion          := "2.10.0-RC5",
+    scalaVersion          := "2.10.0",
     resolvers             ++= Dependencies.resolutionRepos,
     scalacOptions         := Seq(
       "-encoding", "utf8",
