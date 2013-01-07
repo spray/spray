9b42cbbdb4972622dd2f28d148855386e6e199a6
Mathias <mathias@spray.io>
2013-01-07 12:03:55 +0100

    Update echo-server example

--- examples/spray-io/echo-server/src/main/scala/spray/examples/Main.scala
+++ examples/spray-io/echo-server/src/main/scala/spray/examples/Main.scala
@@ -1,54 +1,53 @@
 package spray.examples
 
 import scala.concurrent.duration._
-import akka.actor.{ActorRef, Props, ActorSystem}
+import akka.actor.{Props, ActorSystem}
 import akka.pattern.ask
 import spray.util._
 import spray.io._
+import java.net.InetSocketAddress
+import spray.io.IOBridge.Key
 
 
 object Main extends App {
   // we need an ActorSystem to host our application in
   implicit val system = ActorSystem("echo-server")
 
-  // create and start an IOBridge
-  val ioBridge = IOExtension(system).ioBridge()
-
   // and our actual server "service" actor
-  val server = system.actorOf(
-    Props(new EchoServer(ioBridge)),
-    name = "echo-server"
-  )
+  val server = system.actorOf(Props(new EchoServer), name = "echo-server")
 
   // we bind the server to a port on localhost and hook
   // in a continuation that informs us when bound
   server
     .ask(IOServer.Bind("localhost", 23456))(1 second span)
     .onSuccess { case IOServer.Bound(endpoint, _) =>
-    println("\nBound echo-server to " + endpoint)
-    println("Run `telnet localhost 23456`, type something and press RETURN. Type `STOP` to exit...\n")
-  }
+      println("\nBound echo-server to " + endpoint)
+      println("Run `telnet localhost 23456`, type something and press RETURN. Type `STOP` to exit...\n")
+    }
 }
 
-class EchoServer(ioBridge: ActorRef) extends IOServer(ioBridge) {
+class EchoServer extends IOServer {
+  val ioBridge = IOExtension(context.system).ioBridge()
 
-  override def receive = super.receive orElse {
-    case IOBridge.Received(handle, buffer) =>
-      buffer.array.asString.trim match {
-        case "STOP" =>
-          ioBridge ! IOBridge.Send(handle, BufferBuilder("Shutting down...").toByteBuffer)
-          log.info("Shutting down")
-          context.system.shutdown()
-        case x =>
-          log.debug("Received '{}', echoing ...", x)
-          ioBridge ! IOBridge.Send(handle, buffer, Some('SentOk))
-      }
+  override def bound(endpoint: InetSocketAddress, bindingKey: Key, bindingTag: Any): Receive =
+    super.bound(endpoint, bindingKey, bindingTag) orElse {
 
-    case 'SentOk =>
-      log.debug("Send completed")
+      case IOBridge.Received(handle, buffer) =>
+        buffer.array.asString.trim match {
+          case "STOP" =>
+            ioBridge ! IOBridge.Send(handle, BufferBuilder("Shutting down...").toByteBuffer)
+            log.info("Shutting down")
+            context.system.shutdown()
+          case x =>
+            log.debug("Received '{}', echoing ...", x)
+            ioBridge ! IOBridge.Send(handle, buffer, Some('SentOk))
+        }
 
-    case IOBridge.Closed(_, reason) =>
-      log.debug("Connection closed: {}", reason)
-  }
+      case 'SentOk =>
+        log.debug("Send completed")
+
+      case IOBridge.Closed(_, reason) =>
+        log.debug("Connection closed: {}", reason)
+    }
 
 }
