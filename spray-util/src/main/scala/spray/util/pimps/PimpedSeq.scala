f9cc4836c667575f87da86207eb7ecdd7bc0be95
Mathias <mathias@spray.io>
2013-04-30 11:59:50 +0200

    util: simplify PimpedSeq and remove `mapFindPF` which is equal to existing `collectFirst`

--- spray-util/src/main/scala/spray/util/pimps/PimpedSeq.scala
+++ spray-util/src/main/scala/spray/util/pimps/PimpedSeq.scala
@@ -28,12 +28,6 @@ abstract class PimpedSeq[+A] {
   def mapFind[B](f: A ⇒ Option[B]): Option[B]
 
   /**
-   * Returns the first result of the given partial function when applied to the underlying sequence (in order) or
-   * `None`, if the partial function is not defined for any object of the sequence.
-   */
-  def mapFindPF[B](f: PartialFunction[A, B]): Option[B]
-
-  /**
    * Returns the first object of type B in the underlying sequence or `None`, if none is found.
    */
   def findByType[B: ClassTag]: Option[B] = {
@@ -43,56 +37,23 @@ abstract class PimpedSeq[+A] {
 }
 
 class PimpedLinearSeq[+A](underlying: LinearSeq[A]) extends PimpedSeq[A] {
-
   def mapFind[B](f: A ⇒ Option[B]): Option[B] = {
-    @tailrec
-    def mapFind(seq: LinearSeq[A]): Option[B] = {
+    @tailrec def mapFind(seq: LinearSeq[A]): Option[B] =
       if (!seq.isEmpty) {
-        f(seq.head) match {
-          case x: Some[_] ⇒ x
-          case None       ⇒ mapFind(seq.tail)
-        }
+        val x = f(seq.head)
+        if (x.isEmpty) mapFind(seq.tail) else x
       } else None
-    }
-    mapFind(underlying)
-  }
-
-  def mapFindPF[B](f: PartialFunction[A, B]): Option[B] = {
-    @tailrec
-    def mapFind(seq: LinearSeq[A]): Option[B] = {
-      if (!seq.isEmpty) {
-        if (f.isDefinedAt(seq.head)) Some(f(seq.head))
-        else mapFind(seq.tail)
-      } else None
-    }
     mapFind(underlying)
   }
 }
 
 class PimpedIndexedSeq[+A](underlying: IndexedSeq[A]) extends PimpedSeq[A] {
-
   def mapFind[B](f: A ⇒ Option[B]): Option[B] = {
-    @tailrec
-    def mapFind(ix: Int): Option[B] = {
+    @tailrec def mapFind(ix: Int): Option[B] =
       if (ix < underlying.length) {
-        f(underlying(ix)) match {
-          case x: Some[_] ⇒ x
-          case None       ⇒ mapFind(ix + 1)
-        }
+        val x = f(underlying(ix))
+        if (x.isEmpty) mapFind(ix + 1) else x
       } else None
-    }
-    mapFind(0)
-  }
-
-  def mapFindPF[B](f: PartialFunction[A, B]): Option[B] = {
-    @tailrec
-    def mapFind(ix: Int): Option[B] = {
-      if (ix < underlying.length) {
-        val x = underlying(ix)
-        if (f.isDefinedAt(x)) Some(f(x))
-        else mapFind(ix + 1)
-      } else None
-    }
     mapFind(0)
   }
 }
