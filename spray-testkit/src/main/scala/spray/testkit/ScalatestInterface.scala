4a581ef1b49baa3db1d52e017d9b8cd51cba7512
Mathias <mathias@spray.io>
2013-05-15 16:57:31 +0200

    test-kit: add missing `abstract override` marker in ScalatestInterface

--- spray-testkit/src/main/scala/spray/testkit/ScalatestInterface.scala
+++ spray-testkit/src/main/scala/spray/testkit/ScalatestInterface.scala
@@ -24,8 +24,8 @@ trait ScalatestInterface extends TestFrameworkInterface with BeforeAndAfterAll {
 
   def failTest(msg: String) = throw new TestFailedException(msg, 11)
 
-  override protected def afterAll() { 
-    cleanUp() 
+  abstract override protected def afterAll(): Unit = {
+    cleanUp()
     super.afterAll()
   }
 }
