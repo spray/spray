0fce84d7f299855fb1cdf21c0feba079cf726dc3
Mathias <mathias@spray.io>
2013-06-10 16:38:20 +0200

    Prepare for 1.1-M8 release

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -10,7 +10,7 @@ import ls.Plugin._
 
 
 object BuildSettings {
-  val VERSION = "1.1-SNAPSHOT"
+  val VERSION = "1.1-M8"
 
   lazy val basicSettings = seq(
     version               := NightlyBuildSupport.buildVersion(VERSION),
