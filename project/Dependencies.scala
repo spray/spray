3506e4f868da6f5d10a5392758a87c7a8060ef41
Mathias <mathias@spray.io>
2013-07-09 22:41:12 +0200

    = sbt: upgrade to Akka 2.2.0 final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,9 +13,9 @@ object Dependencies {
   def container (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "container")
 
   val scalaReflect  = "org.scala-lang"                          %   "scala-reflect"               % "2.10.1"
-  val akkaActor     = "com.typesafe.akka"                       %%  "akka-actor"                  % "2.2.0-RC2"
-  val akkaSlf4j     = "com.typesafe.akka"                       %%  "akka-slf4j"                  % "2.2.0-RC2"
-  val akkaTestKit   = "com.typesafe.akka"                       %%  "akka-testkit"                % "2.2.0-RC2"
+  val akkaActor     = "com.typesafe.akka"                       %%  "akka-actor"                  % "2.2.0"
+  val akkaSlf4j     = "com.typesafe.akka"                       %%  "akka-slf4j"                  % "2.2.0"
+  val akkaTestKit   = "com.typesafe.akka"                       %%  "akka-testkit"                % "2.2.0"
   val parboiled     = "org.parboiled"                           %%  "parboiled-scala"             % "1.1.5"
   val shapeless     = "com.chuusai"                             %%  "shapeless"                   % "1.2.4"
   val scalatest     = "org.scalatest"                           %%  "scalatest"                   % "1.9.1"
