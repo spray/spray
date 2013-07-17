4b9a89d2002ee15a6b293fdefe49f674156c921c
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-07-17 10:44:07 +0200

    = sbt: upgrade dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   def runtime   (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "runtime")
   def container (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "container")
 
-  val scalaReflect  = "org.scala-lang"                          %   "scala-reflect"               % "2.10.1"
+  val scalaReflect  = "org.scala-lang"                          %   "scala-reflect"               % "2.10.2"
   val akkaActor     = "com.typesafe.akka"                       %%  "akka-actor"                  % "2.1.4"
   val akkaSlf4j     = "com.typesafe.akka"                       %%  "akka-slf4j"                  % "2.1.4"
   val akkaTestKit   = "com.typesafe.akka"                       %%  "akka-testkit"                % "2.1.4"
@@ -23,11 +23,11 @@ object Dependencies {
   val sprayJson     = "io.spray"                                %%  "spray-json"                  % "1.2.5"
   val twirlApi      = "io.spray"                                %%  "twirl-api"                   % "0.6.2"
   val clHashMap     = "com.googlecode.concurrentlinkedhashmap"  %   "concurrentlinkedhashmap-lru" % "1.3.2"
-  val jettyWebApp   = "org.eclipse.jetty"                       %   "jetty-webapp"                % "8.1.10.v20130312"
+  val jettyWebApp   = "org.eclipse.jetty"                       %   "jetty-webapp"                % "8.1.11.v20130520"
   val servlet30     = "org.eclipse.jetty.orbit"                 %   "javax.servlet"               % "3.0.0.v201112011016" artifacts Artifact("javax.servlet", "jar", "jar")
-  val logback       = "ch.qos.logback"                          %   "logback-classic"             % "1.0.12"
-  val mimepull      = "org.jvnet.mimepull"                      %   "mimepull"                    % "1.9.2"
-  val liftJson      = "net.liftweb"                             %%  "lift-json"                   % "2.5-RC5"
+  val logback       = "ch.qos.logback"                          %   "logback-classic"             % "1.0.13"
+  val mimepull      = "org.jvnet.mimepull"                      %   "mimepull"                    % "1.9.3"
+  val liftJson      = "net.liftweb"                             %%  "lift-json"                   % "2.5.1"
   val json4sNative  = "org.json4s"                              %%  "json4s-native"               % "3.2.4"
   val json4sJackson = "org.json4s"                              %%  "json4s-jackson"              % "3.2.4"
 }
