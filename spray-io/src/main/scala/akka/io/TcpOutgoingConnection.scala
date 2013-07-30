63648f17807035b4c17b5377ec67f5455a31e5a6
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-07-30 12:14:17 +0200

    + io: don't abort connection immediately when `finishConnect` returns false but retry selection, fix #390
     * added a new setting akka.io.tcp.finish-connect-retries which defines
       how often to retry

--- spray-io/src/main/scala/akka/io/TcpOutgoingConnection.scala
+++ spray-io/src/main/scala/akka/io/TcpOutgoingConnection.scala
@@ -9,6 +9,7 @@ import java.nio.channels.{ SelectionKey, SocketChannel }
 import java.net.ConnectException
 import scala.collection.immutable
 import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 import akka.actor.{ ReceiveTimeout, ActorRef }
 import akka.io.Inet.SocketOption
 import akka.io.TcpConnection.CloseInformation
@@ -42,20 +43,32 @@ private[io] class TcpOutgoingConnection(_tcp: TcpExt,
       if (channel.connect(remoteAddress))
         completeConnect(registration, commander, options)
       else
-        context.become(connecting(registration, commander, options))
+        context.become(connecting(registration, commander, options, tcp.Settings.FinishConnectRetries))
   }
 
   def connecting(registration: ChannelRegistration, commander: ActorRef,
-                 options: immutable.Traversable[SocketOption]): Receive = {
+                 options: immutable.Traversable[SocketOption], remainingFinishConnectRetries: Int): Receive = {
     def stop(): Unit = stopWith(CloseInformation(Set(commander), connect.failureMessage))
 
     {
       case ChannelConnectable ⇒
-        if (timeout.isDefined) context.setReceiveTimeout(Duration.Undefined) // Clear the timeout
         try {
-          channel.finishConnect() || (throw new ConnectException(s"Connection to [$remoteAddress] failed"))
-          log.debug("Connection established to [{}]", remoteAddress)
-          completeConnect(registration, commander, options)
+          if (channel.finishConnect()) {
+            if (timeout.isDefined) context.setReceiveTimeout(Duration.Undefined) // Clear the timeout
+            log.debug("Connection established to [{}]", remoteAddress)
+            completeConnect(registration, commander, options)
+          } else {
+            if (remainingFinishConnectRetries > 0) {
+              context.system.scheduler.scheduleOnce(1.millisecond) {
+                channelRegistry.register(channel, SelectionKey.OP_CONNECT)
+              }(context.dispatcher)
+              context.become(connecting(registration, commander, options, remainingFinishConnectRetries - 1))
+            } else {
+              log.debug("Could not establish connection because finishConnect " +
+                "never returned true (consider increasing akka.io.tcp.finish-connect-retries)")
+              stop()
+            }
+          }
         } catch {
           case e: IOException ⇒
             log.debug("Could not establish connection due to {}", e)
