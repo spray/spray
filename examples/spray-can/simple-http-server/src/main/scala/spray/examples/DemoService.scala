faf036d8cefa9fb1f664309d7eb96f4fc825895d
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-08-16 17:53:04 +0200

    = can: add streaming file upload code to simple-http-server example
    This is the most basic form working with the current version of the
    chunking message protocol. Once we have implemented registering for
    chunked messages the `chunkHandlers` map can go.

--- examples/spray-can/simple-http-server/src/main/scala/spray/examples/DemoService.scala
+++ examples/spray-can/simple-http-server/src/main/scala/spray/examples/DemoService.scala
@@ -16,6 +16,10 @@ class DemoService extends Actor with SprayActorLogging {
   implicit val timeout: Timeout = 1.second // for the actor 'asks'
   import context.dispatcher // ExecutionContext for the futures and scheduler
 
+  // a map from connections to chunk handlers
+  // will be created on ChunkedRequestStart and removed on ChunkedRequestEnd
+  var chunkHandlers = Map.empty[ActorRef, ActorRef]
+
   def receive = {
     // when a new connection comes in we register ourselves as the connection handler
     case _: Http.Connected => sender ! Http.Register(self)
@@ -50,6 +54,17 @@ class DemoService extends Actor with SprayActorLogging {
 
     case _: HttpRequest => sender ! HttpResponse(status = 404, entity = "Unknown resource!")
 
+    case s@ChunkedRequestStart(HttpRequest(POST, Uri.Path("/file-upload"), _, _, _)) =>
+      require(!chunkHandlers.contains(sender))
+      val client = sender
+      val handler = context.actorOf(Props(new FileUploadHandler(client, s)))
+      chunkHandlers += (client -> handler)
+      handler.tell(s, client)
+    case c: MessageChunk => chunkHandlers(sender).tell(c, sender)
+    case e: ChunkedMessageEnd =>
+      chunkHandlers(sender).tell(e, sender)
+      chunkHandlers -= sender
+
     case Timedout(HttpRequest(_, Uri.Path("/timeout/timeout"), _, _, _)) =>
       log.info("Dropping Timeout message")
 
@@ -77,6 +92,12 @@ class DemoService extends Actor with SprayActorLogging {
             <li><a href="/timeout/timeout">/timeout/timeout</a></li>
             <li><a href="/stop">/stop</a></li>
           </ul>
+          <p>Test file upload</p>
+          <form action ="/file-upload" enctype="multipart/form-data" method="post">
+            <input type="file" name="datafile" multiple=""></input>
+            <br/>
+            <input type="submit">Submit</input>
+          </form>
         </body>
       </html>.toString()
     )
