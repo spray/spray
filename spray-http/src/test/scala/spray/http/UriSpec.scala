f625b5a68ae26583347673d4e71f9dead69a7223
Mathias <mathias@spray.io>
2013-08-30 16:28:54 +0200

    ! http: add small extensions to Uri model
    Specifically:
    - additional `charset` parameter on `Uri.Host.apply` and `Uri.Query.apply`
    - new method: Uri.Query::toMap

--- spray-http/src/test/scala/spray/http/UriSpec.scala
+++ spray-http/src/test/scala/spray/http/UriSpec.scala
@@ -220,7 +220,7 @@ class UriSpec extends Specification {
   }
 
   "Uri.Query instances" should {
-    def parser(mode: Uri.ParsingMode): String ⇒ Query = Query(_, mode)
+    def parser(mode: Uri.ParsingMode): String ⇒ Query = Query(_, mode = mode)
     "be parsed and rendered correctly in strict mode" in {
       val test = parser(Uri.ParsingMode.Strict)
       test("") === ("", "") +: Query.Empty
