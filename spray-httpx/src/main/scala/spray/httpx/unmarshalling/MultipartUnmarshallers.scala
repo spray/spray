576d67bdce1b335d797d7777ba668295887cc733
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-09-23 17:01:44 +0200

    = httpx: added deprecated MultipartUnmarshallers for better migration experience, see #526

---
+++ spray-httpx/src/main/scala/spray/httpx/unmarshalling/MultipartUnmarshallers.scala
@@ -0,0 +1,6 @@
+package spray.httpx.unmarshalling
+
+@deprecated("Moved to FormDataUnmarshallers", "version after 1.0-M8")
+trait MultipartUnmarshallers extends FormDataUnmarshallers
+@deprecated("Moved to FormDataUnmarshallers", "version after 1.0-M8")
+object MultipartUnmarshallers extends FormDataUnmarshallers
