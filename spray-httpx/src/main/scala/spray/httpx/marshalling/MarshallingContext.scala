10357064f7463a3357f67967553308d3bd5749c2
Mathias <mathias@spray.io>
2013-10-01 15:35:42 +0200

    + httpx: introduce `ToResponseMarshaller` and supporting infrastructure

--- spray-httpx/src/main/scala/spray/httpx/marshalling/MarshallingContext.scala
+++ spray-httpx/src/main/scala/spray/httpx/marshalling/MarshallingContext.scala
@@ -16,8 +16,8 @@
 
 package spray.httpx.marshalling
 
-import spray.http.{ HttpHeader, ContentType, HttpEntity }
 import akka.actor.ActorRef
+import spray.http._
 
 trait MarshallingContext { self ⇒
 
@@ -35,7 +35,7 @@ trait MarshallingContext { self ⇒
   def rejectMarshalling(supported: Seq[ContentType])
 
   /**
-   * Receives the end product entity of a Marshalling.
+   * Receives the HttpEntity produced by a Marshaller.
    * If any headers are given they will be added to the produced HttpMessage.
    */
   def marshalTo(entity: HttpEntity, headers: HttpHeader*)
@@ -69,13 +69,13 @@ trait MarshallingContext { self ⇒
         self.marshalTo(overrideContentType(entity), headers: _*)
       override def startChunkedMessage(entity: HttpEntity, ack: Option[Any], headers: Seq[HttpHeader])(implicit sender: ActorRef) =
         self.startChunkedMessage(overrideContentType(entity), ack, headers)
-      def overrideContentType(entity: HttpEntity) =
+      private def overrideContentType(entity: HttpEntity) =
         entity.flatMap {
-          case HttpEntity.NonEmpty(ct, buf) ⇒
+          case HttpEntity.NonEmpty(ct, data) ⇒
             val c =
               if (contentType.noCharsetDefined && ct.isCharsetDefined) contentType.withCharset(ct.charset)
               else contentType
-            HttpEntity(c, buf)
+            HttpEntity(c, data)
         }
     }
 }
@@ -91,4 +91,65 @@ class DelegatingMarshallingContext(underlying: MarshallingContext) extends Marsh
   def handleError(error: Throwable): Unit = underlying.handleError(error)
   def startChunkedMessage(entity: HttpEntity, ack: Option[Any] = None, headers: Seq[HttpHeader] = Nil)(implicit sender: ActorRef) =
     underlying.startChunkedMessage(entity, ack, headers)
+}
+
+trait ToResponseMarshallingContext { self ⇒
+  /**
+   * Determines whether the given ContentType is acceptable.
+   * If the given ContentType does not define a charset an accepted charset is selected, i.e. the method guarantees
+   * that, if a ContentType instance is returned within the option, it will contain a defined charset.
+   */
+  def tryAccept(contentTypes: Seq[ContentType]): Option[ContentType]
+
+  /**
+   * Signals that the Marshaller rejects the marshalling request because
+   * none of its target ContentTypes is accepted.
+   */
+  def rejectMarshalling(supported: Seq[ContentType])
+
+  /**
+   * Receives the HttpResponse produced by a Marshaller.
+   */
+  def marshalTo(response: HttpResponse)
+
+  /**
+   * Handles the given error.
+   * Calling this method rather than throwing the exception directly allows the error to transcend thread boundaries
+   * and contexts, e.g. when channeling an error from a future execution back to the original scope.
+   */
+  def handleError(error: Throwable)
+
+  /**
+   * Uses the given entity to start a chunked response stream.
+   * The method returns an ActorRef that should be used as the channel for subsequent [[spray.http.MessageChunk]]
+   * instances and the finalizing [[spray.http.ChunkedMessageEnd]].
+   * If a ack is defined it will be sent back to the sender after the initial message part has been successfully
+   * passed to the network.
+   */
+  def startChunkedMessage(response: HttpResponse, ack: Option[Any] = None)(implicit sender: ActorRef): ActorRef
+
+  /**
+   * Creates a new ToResponseMarshallingContext based on this one, that overrides the ContentType of the produced entity
+   * with the given one.
+   */
+  def withContentTypeOverriding(contentType: ContentType): ToResponseMarshallingContext =
+    new ToResponseMarshallingContext {
+      def tryAccept(cts: Seq[ContentType]): Option[ContentType] =
+        Some(if (contentType.isCharsetDefined) cts.head.withCharset(contentType.charset) else cts.head)
+      def rejectMarshalling(supported: Seq[ContentType]): Unit = self.rejectMarshalling(supported)
+      def marshalTo(response: HttpResponse): Unit = self.marshalTo(overrideContentType(response))
+      def handleError(error: Throwable): Unit = self.handleError(error)
+      def startChunkedMessage(response: HttpResponse, ack: Option[Any])(implicit sender: ActorRef): ActorRef =
+        self.startChunkedMessage(overrideContentType(response), ack)
+      private def overrideContentType(response: HttpResponse) =
+        response.withEntity {
+          response.entity.flatMap {
+            case HttpEntity.NonEmpty(ct, data) ⇒
+              val c =
+                if (contentType.noCharsetDefined && ct.isCharsetDefined) contentType.withCharset(ct.charset)
+                else contentType
+              HttpEntity(c, data)
+          }
+        }
+    }
 }
---
+++ spray-httpx/src/main/scala/spray/httpx/marshalling/MetaToResponseMarshallers.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2011-2013 the spray project <http://spray.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package spray.httpx.marshalling
+
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success, Try }
+import spray.http._
+
+trait MetaToResponseMarshallers {
+
+  implicit def optionMarshaller[T](implicit m: ToResponseMarshaller[T]) =
+    ToResponseMarshaller[Option[T]] { (value, ctx) ⇒
+      value match {
+        case Some(v) ⇒ m(v, ctx)
+        case None    ⇒ ctx.marshalTo(HttpResponse(StatusCodes.NotFound))
+      }
+    }
+
+  implicit def eitherMarshaller[A, B](implicit ma: ToResponseMarshaller[A], mb: ToResponseMarshaller[B]) =
+    ToResponseMarshaller[Either[A, B]] { (value, ctx) ⇒
+      value match {
+        case Left(a)  ⇒ ma(a, ctx)
+        case Right(b) ⇒ mb(b, ctx)
+      }
+    }
+
+  implicit def futureMarshaller[T](implicit m: ToResponseMarshaller[T], ec: ExecutionContext) =
+    ToResponseMarshaller[Future[T]] { (value, ctx) ⇒
+      value.onComplete {
+        case Success(v)     ⇒ m(v, ctx)
+        case Failure(error) ⇒ ctx.handleError(error)
+      }
+    }
+
+  implicit def tryMarshaller[T](implicit m: ToResponseMarshaller[T]) =
+    ToResponseMarshaller[Try[T]] { (value, ctx) ⇒
+      value match {
+        case Success(v) ⇒ m(v, ctx)
+        case Failure(t) ⇒ ctx.handleError(t)
+      }
+    }
+}
+
+object MetaToResponseMarshallers extends MetaToResponseMarshallers
