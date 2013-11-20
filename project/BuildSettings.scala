e6b5be8fa176553269c3a84526f85dabf74a259e
Mathias <mathias@spray.io>
2013-11-20 16:16:12 +0100

    = project: bump version to 1.0-RC4

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -10,7 +10,7 @@ import com.typesafe.sbt.osgi.SbtOsgi
 import SbtOsgi._
 
 object BuildSettings {
-  val VERSION = "1.0-RC3"
+  val VERSION = "1.0-RC4"
 
   lazy val basicSettings = seq(
     version               := NightlyBuildSupport.buildVersion(VERSION),
