529acc355505859afe2875a9e08195ba0ef20e69
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-11-28 13:04:40 +0100

    = docs: ExecutionDirectives documentation

---
+++ docs/documentation/spray-routing/code/docs/directives/ExecutionDirectivesExamplesSpec.scala
@@ -0,0 +1,134 @@
+package docs.directives
+
+import spray.routing.{RejectionHandler, ExceptionHandler, Rejection, Directive1}
+import spray.http.StatusCodes
+
+class ExecutionDirectivesExamplesSpec extends DirectivesSpec {
+  import scala.concurrent.ExecutionContext.global
+  "detach-0" in {
+    val route =
+      detach() {
+        complete("Result") // route executed in future
+      }
+    Get("/") ~> route ~> check {
+      responseAs[String] === "Result"
+    }
+  }
+  "detach-1" in {
+    /// / a custom directive to extract the id of the current thread
+    def currentThreadId: Directive1[Long] = extract(_ => Thread.currentThread().getId)
+    val route =
+      currentThreadId { originThread =>
+        path("rejectDetached") {
+          detach() {
+            reject()
+          }
+        } ~
+        path("reject") {
+          reject()
+        } ~
+        currentThreadId { alternativeThread =>
+          complete(s"$originThread,$alternativeThread")
+        }
+      }
+
+    Get("/reject") ~> route ~> check {
+      val Array(original, alternative) = responseAs[String].split(",")
+      original === alternative
+    }
+    Get("/rejectDetached") ~> route ~> check {
+      val Array(original, alternative) = responseAs[String].split(",")
+      original !== alternative
+    }
+  }
+  "dynamic-0" in {
+    var value = 0
+    val route =
+      dynamic {
+        value += 1 /// executed for each request
+        complete(s"Result is now $value") // route executed in future
+      }
+    Get("/") ~> route ~> check {
+      responseAs[String] === "Result is now 1"
+    }
+    Get("/") ~> route ~> check {
+      responseAs[String] === "Result is now 2"
+    }
+  }
+  "dynamicIf" in {
+    def countDynamically(dyn: Boolean) = {
+      var value = 0
+      dynamicIf(dyn) {
+        value += 1 /// executed for each request
+        complete(s"Result is now $value") // route executed in future
+      }
+    }
+
+    val route =
+      path("dynamic")(countDynamically(true)) ~
+      path("static")(countDynamically(false))
+
+    Get("/dynamic") ~> route ~> check {
+      responseAs[String] === "Result is now 1"
+    }
+    Get("/dynamic") ~> route ~> check {
+      responseAs[String] === "Result is now 2"
+    }
+    Get("/dynamic") ~> route ~> check {
+      responseAs[String] === "Result is now 3"
+    }
+
+    Get("/static") ~> route ~> check {
+      responseAs[String] === "Result is now 1"
+    }
+    Get("/static") ~> route ~> check {
+      responseAs[String] === "Result is now 1"
+    }
+    Get("/static") ~> route ~> check {
+      responseAs[String] === "Result is now 1"
+    }
+  }
+  "handleExceptions" in {
+    val divByZeroHandler = ExceptionHandler {
+      case _: ArithmeticException => complete(StatusCodes.BadRequest, "You've got your arithmetic wrong, fool!")
+    }
+    val route =
+      path("divide" / IntNumber / IntNumber) { (a, b) =>
+        handleExceptions(divByZeroHandler) {
+          complete(s"The result is ${a / b}")
+        }
+      }
+
+    Get("/divide/10/5") ~> route ~> check {
+      responseAs[String] === "The result is 2"
+    }
+    Get("/divide/10/0") ~> route ~> check {
+      status === StatusCodes.BadRequest
+      responseAs[String] === "You've got your arithmetic wrong, fool!"
+    }
+  }
+  "handleRejections" in {
+    val totallyMissingHandler = RejectionHandler {
+      case Nil /* secret code for path not found */ =>
+        complete(StatusCodes.NotFound, "Oh man, what you are looking for is long gone.")
+    }
+    val route =
+      pathPrefix("handled") {
+        handleRejections(totallyMissingHandler) {
+          path("existing")(complete("This path exists"))
+        }
+      }
+
+    Get("/handled/existing") ~> route ~> check {
+      responseAs[String] === "This path exists"
+    }
+    Get("/missing") ~> sealRoute(route) /* applies default handler */ ~> check {
+      status === StatusCodes.NotFound
+      responseAs[String] === "The requested resource could not be found."
+    }
+    Get("/handled/missing") ~> route ~> check {
+      status === StatusCodes.NotFound
+      responseAs[String] === "Oh man, what you are looking for is long gone."
+    }
+  }
+}
--- docs/documentation/spray-routing/execution-directives/handleRejections.rst
+++ docs/documentation/spray-routing/execution-directives/handleRejections.rst
@@ -3,4 +3,24 @@
 handleRejections
 ================
 
-(todo)
+Handles rejections produced by the inner route and handles them using the specified ``RejectionHandler``.
+
+Signature
+---------
+
+.. includecode:: /../spray-routing/src/main/scala/spray/routing/directives/ExecutionDirectives.scala
+   :snippet: handleRejections
+
+Description
+-----------
+
+Using this directive is an alternative to using a global implicily defined ``RejectionHandler`` that
+applies to the complete route.
+
+See :ref:`Rejections` for general information about options for handling rejections.
+
+Example
+-------
+
+.. includecode:: ../code/docs/directives/ExecutionDirectivesExamplesSpec.scala
+   :snippet: handleRejections
