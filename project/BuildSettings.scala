3c32fbbd92ea90ea5fc2dad21c00a23c62095260
Mathias <mathias@spray.io>
2013-12-19 09:23:16 +0100

    = project: update CHANGELOG and bump version to 1.3-M2

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -9,7 +9,7 @@ import com.typesafe.sbt.osgi.SbtOsgi
 import SbtOsgi._
 
 object BuildSettings {
-  val VERSION = "1.3-M1"
+  val VERSION = "1.3-M2"
 
   lazy val basicSettings = seq(
     version               := NightlyBuildSupport.buildVersion(VERSION),
