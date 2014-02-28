d8f35813069bdc09cbd9a13b10f0bb6010093409
Mathias <mathias@spray.io>
2014-02-28 11:58:34 +0100

    = project: update akka to 2.3.0-RC4, bump version, update CHANGELOG

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -9,7 +9,7 @@ import com.typesafe.sbt.osgi.SbtOsgi
 import SbtOsgi._
 
 object BuildSettings {
-  val VERSION = "1.3-RC1"
+  val VERSION = "1.3-RC4"
 
   lazy val basicSettings = seq(
     version               := NightlyBuildSupport.buildVersion(VERSION),
