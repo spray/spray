24f9baca5d0c9a73c1a51fa62635bba5b5b06db4
Mathias <mathias@spray.io>
2014-03-14 15:43:43 +0100

    = project: bump version to 1.2.1

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -10,7 +10,7 @@ import com.typesafe.sbt.osgi.SbtOsgi
 import SbtOsgi._
 
 object BuildSettings {
-  val VERSION = "1.2.0"
+  val VERSION = "1.2.1"
 
   lazy val basicSettings = seq(
     version               := NightlyBuildSupport.buildVersion(VERSION),
