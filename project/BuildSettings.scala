869bdbd802788354fabd3f8f36d0593336a84e3f
Mathias <mathias@spray.io>
2014-04-04 15:09:11 +0200

    = project: upgrade to Scala 2.10.4

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -24,7 +24,7 @@ object BuildSettings {
                              "web services on top of Akka",
     startYear             := Some(2011),
     licenses              := Seq("Apache 2" -> new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")),
-    scalaVersion          := "2.10.3",
+    scalaVersion          := "2.10.4",
     resolvers             ++= Dependencies.resolutionRepos,
     scalacOptions         := Seq(
       "-encoding", "utf8",
