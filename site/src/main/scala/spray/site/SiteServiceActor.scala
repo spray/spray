458ca672f68a5e11a71b76710eb9c810a85965ff
Mathias <mathias@spray.io>
2014-05-09 17:03:21 +0200

    = site: add redirection from waves.io

--- site/src/main/scala/spray/site/SiteServiceActor.scala
+++ site/src/main/scala/spray/site/SiteServiceActor.scala
@@ -59,6 +59,9 @@ class SiteServiceActor(settings: SiteSettings) extends HttpServiceActor with Sea
         host(_.endsWith("pegdown.org")) {
           redirect("https://github.com/sirthias/pegdown", Found)
         } ~
+        host(x => x.endsWith("waves.io") | x.endsWith("streamed.io")) {
+          redirect("https://github.com/sirthias/waves", Found)
+        } ~
         host("spray.io", "localhost", "127.0.0.1") {
           path("favicon.ico") {
             complete(NotFound) // fail early in order to prevent error response logging
