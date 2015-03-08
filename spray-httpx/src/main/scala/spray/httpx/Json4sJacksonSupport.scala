88d6acd2dd7dcb961cba5a4160b2899df60f79a2
Julian Tescher <jatescher@gmail.com>
2015-03-08 10:46:21 -0700

    Update copyright year to 2015

--- spray-httpx/src/main/scala/spray/httpx/Json4sJacksonSupport.scala
+++ spray-httpx/src/main/scala/spray/httpx/Json4sJacksonSupport.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2011-2013 the spray project <http://spray.io> <http://spray.io>
+ * Copyright © 2011-2015 the spray project <http://spray.io> <http://spray.io>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
