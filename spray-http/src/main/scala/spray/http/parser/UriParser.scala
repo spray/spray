9504170abdb43deb8af7f04405fce56835ca803d
Wojciech Jurczyk <wojtek.jurczyk@gmail.com>
2015-03-16 23:01:34 +0100

    = http: #1014 Fix handling of empty authority in Uris

--- spray-http/src/main/scala/spray/http/parser/UriParser.scala
+++ spray-http/src/main/scala/spray/http/parser/UriParser.scala
@@ -239,8 +239,8 @@ private[http] class UriParser(input: ParserInput, charset: Charset, mode: Uri.Pa
     while (matches(UNRESERVED & ~UPPER_ALPHA | SUB_DELIM) || UpperAlpha || `pct-encoded`) mark = cursor
     reset(mark)
     // host is always decoded using UTF-8 (http://tools.ietf.org/html/rfc3986#section-3.2.2)
-    _host = NamedHost {
-      if (cursor > start) {
+    _host = if (cursor > start) {
+      NamedHost {
         firstUpper -= start
         firstPercent -= start
         val s = slice(start, cursor)
@@ -248,8 +248,8 @@ private[http] class UriParser(input: ParserInput, charset: Charset, mode: Uri.Pa
           if (firstPercent >= 0) toLowerIfNeeded(decodeIfNeeded(s, firstPercent, charset), math.min(firstPercent, firstUpper))
           else toLowerIfNeeded(s, firstUpper)
         else if (firstPercent >= 0) toLowerIfNeeded(decodeIfNeeded(s, firstPercent, charset), firstPercent) else s
-      } else ""
-    }
+      }
+    } else Host.Empty
     true
   }
 
